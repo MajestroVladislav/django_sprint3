2f783fccc7fea96abfc7ff26a55aabc03b274367
MajestroVladislav <vlakal1nin@yandex.ru>
2025-11-27 00:09:39 +0400

    Хм...

--- blogicum/utils/debug_patch.py
+++ blogicum/utils/debug_patch.py
@@ -5,39 +5,48 @@ import sys
 import logging
 
 logger = logging.getLogger(__name__)
+# Убедимся, что логгер выводит в stdout, чтобы Практикум его поймал
 handler = logging.StreamHandler(sys.stdout)
 handler.setLevel(logging.DEBUG)
 logger.addHandler(handler)
 logger.setLevel(logging.DEBUG)
 
-_original_dict = builtins.dict
-_original_update = dict.update
+# Сохраняем оригинальный тип dict
+_original_dict_type = builtins.dict
 
 
-def _safe_dict(arg=(), **kwargs):
-    try:
-        return _original_dict(arg, **kwargs)
-    except Exception as e:  # ловим ValueError и прочие
-        logger.error("DEBUG_PATCH: dict(...) raised %s", e)
-        logger.error("DEBUG_PATCH: problematic arg repr: %r", arg)
-        logger.error("DEBUG_PATCH: kwargs repr: %r", kwargs)
-        logger.error("DEBUG_PATCH: TRACEBACK:\\n%s",
-                     "".join(traceback.format_stack()))
-        raise
+class DebugDict(_original_dict_type):
+    """
+    Подкласс dict, который перехватывает ошибки при создании
+    и обновлении,
+    логирует traceback и проблемные аргументы.
+    """
+
+    def __init__(self, *args, **kwargs):
+        try:
+            super().__init__(*args, **kwargs)
+        except Exception as e:
+            logger.error("DEBUG_PATCH: DebugDict.__init__(...) raised %s",
+                         e)
+            logger.error("DEBUG_PATCH: problematic args: %r, kwargs: %r",
+                         args, kwargs)
+            logger.error("DEBUG_PATCH: TRACEBACK:\\n%s",
+                         "".join(traceback.format_stack()))
+            raise
+
+    def update(self, *args, **kwargs):
+        try:
+            super().update(*args, **kwargs)
+        except Exception as e:
+            logger.error("DEBUG_PATCH: DebugDict.update(...) raised %s",
+                         e)
+            logger.error("DEBUG_PATCH: self: %r",
+                         self)
+            logger.error("DEBUG_PATCH: problematic args: %r, kwargs: %r",
+                         args, kwargs)
+            logger.error("DEBUG_PATCH: TRACEBACK:\\n%s",
+                         "".join(traceback.format_stack()))
+            raise
 
 
-def _safe_update(self, *args, **kwargs):
-    try:
-        return _original_update(self, *args, **kwargs)
-    except Exception as e:
-        logger.error("DEBUG_PATCH: dict.update(...) raised %s", e)
-        logger.error("DEBUG_PATCH: self repr: %r", self)
-        logger.error("DEBUG_PATCH: args repr: %r", args)
-        logger.error("DEBUG_PATCH: kwargs repr: %r", kwargs)
-        logger.error("DEBUG_PATCH: TRACEBACK:\\n%s",
-                     "".join(traceback.format_stack()))
-        raise
-
-
-builtins.dict = _safe_dict
-dict.update = _safe_update
+builtins.dict = DebugDict
