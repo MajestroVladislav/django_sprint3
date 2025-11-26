eebd72af4c1ee0e43527abd08c08337b71b5e061
MajestroVladislav <vlakal1nin@yandex.ru>
2025-11-26 23:43:14 +0400

    ////

--- blogicum/blogicum/settings.py
+++ blogicum/blogicum/settings.py
@@ -132,7 +132,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'html'
+    BASE_DIR.parent / 'html'
 ]
 
 # Default primary key field type
