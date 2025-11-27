b23038ff5676d1f2fffe98554c4c935409f394ee
MajestroVladislav <vlakal1nin@yandex.ru>
2025-11-27 05:43:41 +0400

    ыррр

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -11,9 +11,11 @@ def index(request):
     ).order_by('-pub_date')[:5]
     return render(request, 'blog/index.html', {'posts': posts})
 
+
 def post_detail(request, id):
     post = get_object_or_404(Post, pk=id)
     return render(request, 'blog/detail.html', {'post': post})
+
 
 def category_posts(request, slug):
     category = get_object_or_404(Category, slug=slug,
